3e68cbac6b15b62340f50b738031bb4806e70571
Le Huan <lehuannhatrang98@gmail.com>
2019-04-29 21:06:31 +0700

    fix package.json and bugs

--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,11 @@ import UserModel from './user.model';
 import UserInfoModel from './user.info.model';
 import UserActionModel from './user.action.model';
 import QuestionModel from './quiz.question.model';
-import QuizModel from './quiz.model';
+// import QuizModel from './quiz.model';
 export {
     UserModel,
     UserInfoModel,
     UserActionModel,
     QuestionModel,
-    QuizModel
+    // QuizModel
 };
